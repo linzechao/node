dbd6649675f30e73a66b45c805350b470efcd59d
Mr. Super <linzechao44@163.com>
2015-07-15 21:11:10 +0800

    完善node的url模块

--- base/url.js
+++ base/url.js
@@ -1,8 +1,27 @@
 var url = require('url');
-var location = 'http://www.super.com:8090/family?brother=chao&age=18#about';
+
+var location = 'http://user:super@www.super.com:8090/family?brother=chao&age=18#about';
+
+var json = {
+	protocol: 'http:',
+	slashes: true,
+  	auth: 'user:super',
+  	host: 'www.super.com:8090',
+  	port: '8090',
+  	hostname: 'www.super.com',
+  	hash: '#about',
+  	search: '?brother=chao&age=18',
+  	query: 'brother=chao&age=18',
+  	pathname: '/family',
+  	path: '/family?brother=chao&age=18',
+  	href: 'http://user:super@www.super.com:8090/family?brother=chao&age=18#about'
+};
 
 // console.info(url.parse(location));
 // console.info(url.parse(location, true));
 // console.info(url.parse(location, true, true));
 
-console.info(url.parse(location));
+// console.info(url.format(json));
+console.info(url.resolve('/one/two/three', 'four'));
+console.info(url.resolve('http://example.com/', '/one'));
+console.info(url.resolve('http://example.com/one/two/three', '/one'));
