56dc8bf6ee99330b34747c0f488f0e4c625721d9
Mr. Super <linzechao44@163.com>
2015-07-16 07:51:06 +0800

    添加Node的events模块，以及完善爬虫功能

---
+++ base/crawler.js
@@ -0,0 +1,50 @@
+/**
+ * 小小爬虫器
+ */
+
+var http = require('http'),
+	cheerio = require('cheerio'), // npm install cheerio
+	url = 'http://expo.bootcss.com/';
+
+function filterChapters(html) {
+	var $ = cheerio.load(html),
+		$thumbnail = $('.thumbnail'),
+		imgs = [];
+
+	$thumbnail.each(function(index, item) {
+		var $this = $(this),
+			href = $this.attr('href'),
+			title = $this.find('img').attr('alt'),
+			img = $this.find('img').attr('src');
+
+		imgs.push({
+			href: href,
+			title: title,
+			img: img
+		});
+	});
+
+	return imgs;
+}
+
+http.get(url, function(res) {
+	var html = '';
+
+	res.on('data', function(data) {
+		html += data;
+	});
+
+	res.on('end', function() {
+		// console.log(html);
+		var imgs = filterChapters(html);
+
+		imgs.forEach(function(item) {
+			console.log('<a href="' + item.href + '"><img src="'
+				+ item.img + '" title="'
+				+ item.title + '"></a>\n');
+		});
+	});
+
+}).on('error', function() {
+	console.log('爬虫获取数据出错....');
+});
---
+++ base/events.js
@@ -0,0 +1,64 @@
+var EventEmitter = require('events').EventEmitter,
+	person = new EventEmitter();
+
+person.setMaxListeners(11); // 设置事件监听个数最大值
+
+// 也可以使用addListener
+person.on('下班', function(who) {
+	console.log(who + '提交代码');
+});
+
+person.on('下班', function(who) {
+	console.log(who + '电脑关机');
+});
+
+person.on('下班', function(who) {
+	console.log(who + '玩会手机');
+});
+
+person.on('下班', function(who) {
+	console.log(who + '收拾东西');
+});
+
+person.on('下班', function(who) {
+	console.log(who + '记得打卡');
+});
+
+person.on('下班', function(who) {
+	console.log(who + '上个厕所');
+});
+
+person.on('下班', function(who) {
+	console.log(who + '坐下电梯');
+});
+
+person.on('下班', function(who) {
+	console.log(who + '挪出单车');
+});
+
+person.on('下班', function(who) {
+	console.log(who + '买肉买菜');
+});
+
+function man(who) {
+	console.log(who + '炒菜做饭');
+}
+
+person.on('下班', man);
+
+person.on('下班', function(who) {
+	console.log(who + '洗澡睡觉');
+});
+
+// 删除事件监听
+person.removeListener('下班', man);
+// person.removeAllListeners(); // 删除所有事件监听
+// person.removeAllListeners('下班'); // 删除该事件所有监听
+
+var hasOffWorkListener = person.emit('下班', 'Super'); // 触发事件
+// console.log(person.listeners('下班')); // 返回该事件数组
+console.log(person.listeners('下班').length); // 返回该事件条数
+// 类方法，静态方法
+console.log(EventEmitter.listenerCount(person, '下班')); // 同上
+
+console.log('是否有下班这个监听事件：' + hasOffWorkListener);
