2efed6691646ad7b0983bd3c31a7b88b7a75bb5f
Mr. Super <linzechao44@163.com>
2015-07-16 23:37:08 +0800

    完善Node的events模块

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 	1、console		// 100%
 	2、url			// 100%
 	3、querystring	// 100%
-	4、events		// 80%
+	4、events		// 100%
 	5、http			// 10%
 ```
--- base/events.js
+++ base/events.js
@@ -1,6 +1,7 @@
 var EventEmitter = require('events').EventEmitter,
 	person = new EventEmitter();
 
+// 设置0的话，不限制个数
 person.setMaxListeners(11); // 设置事件监听个数最大值
 
 // 也可以使用addListener
@@ -55,10 +56,24 @@ person.removeListener('下班', man);
 // person.removeAllListeners(); // 删除所有事件监听
 // person.removeAllListeners('下班'); // 删除该事件所有监听
 
-var hasOffWorkListener = person.emit('下班', 'Super'); // 触发事件
+// var hasOffWorkListener = person.emit('下班', 'Super'); // 触发事件
 // console.log(person.listeners('下班')); // 返回该事件数组
-console.log(person.listeners('下班').length); // 返回该事件条数
+// console.log(person.listeners('下班').length); // 返回该事件条数
 // 类方法，静态方法
-console.log(EventEmitter.listenerCount(person, '下班')); // 同上
+// console.log(EventEmitter.listenerCount(person, '下班')); // 同上
 
-console.log('是否有下班这个监听事件：' + hasOffWorkListener);
+// console.log('是否有下班这个监听事件：' + hasOffWorkListener);
+
+person.once('纸巾', function(who) {
+	console.log(who + '纸巾擦汗, 扔掉。');
+});
+
+/*
+var hasTissue1 = person.emit('纸巾', 'Super');
+var hasTissue2 = person.emit('纸巾', 'Super'); // 不在执行
+
+console.log(hasTissue1); // true
+console.log(hasTissue2); // false，执行过后删除事件
+*/
+
+console.log(person.defaultMaxListeners); // undefined??
