2e93d936d6261d3f1dac7f6e57d50094c99d2302
Mr. Super <linzechao44@163.com>
2015-07-21 21:23:41 +0800

    完善net模块

--- base/net/socket.js
+++ base/net/socket.js
@@ -11,15 +11,26 @@ socket.connect(8090, function(conn) {
 
 	// socket.pause(); // 停止读取数据
 	// socket.resume(); // 恢复读取数据
+	// socket.address(); // 连接详情
+	// console.log(socket.remoteAddress); // 远程IP详情
+	// console.log(socket.remotePort); // 远程端口
+	// console.log(socket.localAddress); // 本地IP详情
+	// console.log(socket.localPort); // 本地端口
+	// console.log(socket.byteRead); // 所接收的字节数
+	// console.log(socket.bytesWritten); // 所发送的字节数
+});
 
-	socket.on('data', function(data) {
-		console.log('服务端说：' + data);
-	});
+socket.on('data', function(data) {
+	console.log('服务端说：' + data);
 });
 
 socket.on('error', function() {
 	console.log('报错咯....');
 	socket.destroy(); // 销毁
+});
+
+socket.on('close', function() {
+	console.log('close'); // 在end方法后触发
 });
 
 socket.on('end', function() {
@@ -29,4 +40,29 @@ socket.on('end', function() {
 socket.setTimeout(1000, function() { // 定时，为0时，禁用闲置超时
 	socket.end('this is timeout function!');
 });
+
+
+// socket.setNoDelay(false); // 禁用纳格算法(Nagle)
+// socket.setKeepAlive(false); // 禁用长连接
+// socket.unref(); // 退出连接
+// socket.ref(); // 恢复连接
+
+socket.on('drain', function() {
+	console.log('drain'); // 缓冲区被清除时触发
+});
+
+
+/*
+socket.on('lookup', function(err, address, family) { // 连接前执行
+	console.log(err);
+	console.log(address);
+	console.log(family);
+});
+*/
+
+socket.on('timeout', function() {
+	console.log('timeout'); // setTimeout执行后触发
+});
+
+
 
