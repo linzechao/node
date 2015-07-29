e2d3fffad2537676bd8e3e414180670f354ceef6
Mr. Super <linzechao44@163.com>
2015-07-29 22:20:17 +0800

    添加domain模块

--- base/domain.js
+++ base/domain.js
@@ -1,2 +1,19 @@
-domain.js
+var domain = require('domain');
+// console.log(domain);
+
+var d = domain.create();
+d.on('error', function(er) {
+  console.error('Caught error!', er);
+});
+
+d.run(function() {
+  process.nextTick(function() {
+    setTimeout(function() { // 模拟几个不同的异步的东西
+      fs.open('non-existent file', 'r', function(er, fd) {
+        if (er) throw er;
+        // 继续。。。
+      });
+    }, 100);
+  });
+});
 
