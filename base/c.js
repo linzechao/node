a19b55bc86e374511cc7f68968e235addd07cf45
Mr. Super <linzechao44@163.com>
2015-07-29 23:43:51 +0800

    添加fs模块

--- base/c.js
+++ base/c.js
@@ -1,4 +1,4 @@
-var obj2 = createObject(20);
-console.log( obj2.plusOne() ); // 21
-console.log( obj2.plusOne() ); // 22
-console.log( obj2.plusOne() ); // 23
+var obj = new addon.MyObject(10);
+console.log( obj.plusOne() ); // 11
+console.log( obj.plusOne() ); // 12
+console.log( obj.plusOne() ); // 13
---
+++ base/fs/fs.js
@@ -0,0 +1,35 @@
+// 文件操作
+var fs = require('fs');
+// console.log(fs);
+
+// 所有异步方法后面加上Sync即可换成同步方法
+
+/*
+// 异步删除一个文件
+fs.unlink('./base/c.js', function (err) {
+  if (err) throw err;
+  console.log('successfully deleted ./base/c.js');
+});
+// 同步删除一个文件
+fs.unlinkSync('/tmp/hello');
+console.log('successfully deleted /tmp/hello');
+*/
+
+// 删除第一个文件
+// 将第一个文件的内容复制到第二个文件
+/*fs.rename('./base/fs/old.txt', './base/fs/new.txt', function(err) {
+	if (err) {
+		throw err;
+	}
+
+	// 输出该文件相关信息
+	fs.stat('./base/fs/new.txt', function(err, stats) {
+		if (err) {
+			throw err;
+		}
+
+		console.log('stat: ' + JSON.stringify(stats));
+	});
+});*/
+
+
