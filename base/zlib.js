699d8ac1ff41591ee60cfa5befd18a92351a4f16
Mr. Super <linzechao44@163.com>
2015-07-30 08:04:13 +0800

    添加zlib模块

---
+++ base/zlib.js
@@ -0,0 +1,10 @@
+var zlib = require('zlib');
+// console.log(zlib);
+
+var buffer = new Buffer('eJzT0yMAAGTvBe8=', 'base64');
+zlib.unzip(buffer, function(err, buffer) {
+  if (!err) {
+    console.log(buffer.toString());
+  }
+});
+
