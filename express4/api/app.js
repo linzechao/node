0cd6b3d17c225f5a115784f0ea56a6ca72cd1086
Mr. Super <linzechao44@163.com>
2015-08-12 09:15:29 +0800

    添加express示例

--- express4/api/app.js
+++ express4/api/app.js
@@ -1,3 +1,19 @@
-var express = require('express');
-console.log(express);
+var express = require('express'),
+	app = express();
+
+app.get('/', function(req, res) {
+	// console.log('Hello Server!');
+	res.sendfile('./index.html');
+});
+
+app.listen(3000);
+
+app.locals.title = 'Mr. Super';
+app.locals.email = 'linzechao44@163.com';
+
+// console.log(app.locals.title);
+// console.log(app.locals.email);
+
+
+
 
