841d6b01cfdbe5ca5e38a2a226db4c103cd18fa0
Mr. Super <linzechao44@163.com>
2015-08-12 22:06:32 +0800

    添加middleware中间件

--- express4/api/control/middleware.js
+++ express4/api/control/middleware.js
@@ -40,5 +40,49 @@ app.get('/user/:id', function (req, res, next) {
 
 
 // 路由级中间件
+var router = express.Router();
+
+// 没有挂载路径的中间件，通过该路由的每个请求都会执行该中间件
+router.use(function (req, res, next) {
+	console.log('Time: ', Date.now());
+	next();
+});
+
+
+// 错误处理中间件
+/*
+错误处理中间件有 4 个参数，
+定义错误处理中间件时必须使用这 4 个参数。
+即使不需要 next 对象，也必须在签名中声明它，
+否则中间件会被识别为一个常规中间件，不能处理错误。
+*/
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+
+// 内置中间件
+// static是唯一一个内置中间件
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html'],
+  index: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now());
+  }
+}
+
+app.use(express.static('public', options));
+
+
+// 第三方中间件
+var cookieParser = require('cookie-parser');
+// 加载用于解析 cookie 的中间件
+app.use(cookieParser());
+
 
 
